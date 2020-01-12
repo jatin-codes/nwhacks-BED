b371436318c7b325025a98f11fba5c743efad507
mrhuytran <tranquanghuy2602@gmail.com>
2020-01-12 11:39:07 -0800

    added get_user

--- nwhacks20_DEV/sql.py
+++ nwhacks20_DEV/sql.py
@@ -1,4 +1,4 @@
-# import mysql.connector
+import mysql.connector
 
 class SQL:
 
--- nwhacks20_DEV/views.py
+++ nwhacks20_DEV/views.py
@@ -21,8 +21,7 @@ def detect_similar_face(request, face_id, dectected_face_ids):
 
 
 def add_friends(request):
-    data = json.loads(request.body)
-    img_url = data.get('image_url')
+    img_url = 'https://lh3.googleusercontent.com/EQS5SSPak-cL_eNz0nyUPn_HLyHabOwx4X9xRUkDxBuqAYxpVmr53ul68Ekyhz6NFWPMOVKvv4bGo3pCvzIiWEvywCm_kJxWSFhC2Dg7CJBBsUjHLBr1pZa4UcAhBmJLoysJx3WDutv4k3QnGTqoBLRbJREeIC3o6BY1rNFcVcvbJVtiwnpilGpF5CJsUN6Og6qp2QWX9AZ9YNwbEYITQL7jylLHvhGtPj_2_yoV3uBKvne-tZ67EQnwI-O985QDrtXxIPAQ_wGZev0NwUp89q545G_9ojp0HVedCzDeODTyGbisN7C15cAracEP0yJyt7EPhl0Ab7EWB-lQWtYE_3SEzKVG9L5fd0BEjU25qkVN6yh8MbmcaNNtGEADoxrMbkZiN86ZNA4w26iiWmfASDxvU3j9H6UYAsppeKujCTJBcAUJh6JpvQeAAvKcDJy2d_neFFgz7cJOypYjocla2sGv8-ts9zDqMge-ihZxuyz5OGC6olbaTSD71CbMzIxizerAfnYbkntjNsaHGE5s9dqf55oxc7duCZSG7S9XQfN0gLLEZdukzYnkYjDAzcUYyD8iBUdn01tFMZGbVwOFDxZ81E6LoeM0SLO33a8Y4dXGAiSGBHSEPRPbOVzYhxDWhzEpoUN8rLAplI76_eH8ZwkvmVruT2RseOBleEplgeIm7Tl2aWbgYao=w1428-h1903-no'
 
     face_ids_from_img = face_reg.detect_faces(img_url)
     face_ids_from_db = sql.get_face_ids()
@@ -39,8 +38,15 @@ def add_friends(request):
         sql.insert_friend_record(name, face_id)
     return HttpResponse('')
 
-
-
+def get_user(request):
+    img_url = 'https://lh3.googleusercontent.com/EQS5SSPak-cL_eNz0nyUPn_HLyHabOwx4X9xRUkDxBuqAYxpVmr53ul68Ekyhz6NFWPMOVKvv4bGo3pCvzIiWEvywCm_kJxWSFhC2Dg7CJBBsUjHLBr1pZa4UcAhBmJLoysJx3WDutv4k3QnGTqoBLRbJREeIC3o6BY1rNFcVcvbJVtiwnpilGpF5CJsUN6Og6qp2QWX9AZ9YNwbEYITQL7jylLHvhGtPj_2_yoV3uBKvne-tZ67EQnwI-O985QDrtXxIPAQ_wGZev0NwUp89q545G_9ojp0HVedCzDeODTyGbisN7C15cAracEP0yJyt7EPhl0Ab7EWB-lQWtYE_3SEzKVG9L5fd0BEjU25qkVN6yh8MbmcaNNtGEADoxrMbkZiN86ZNA4w26iiWmfASDxvU3j9H6UYAsppeKujCTJBcAUJh6JpvQeAAvKcDJy2d_neFFgz7cJOypYjocla2sGv8-ts9zDqMge-ihZxuyz5OGC6olbaTSD71CbMzIxizerAfnYbkntjNsaHGE5s9dqf55oxc7duCZSG7S9XQfN0gLLEZdukzYnkYjDAzcUYyD8iBUdn01tFMZGbVwOFDxZ81E6LoeM0SLO33a8Y4dXGAiSGBHSEPRPbOVzYhxDWhzEpoUN8rLAplI76_eH8ZwkvmVruT2RseOBleEplgeIm7Tl2aWbgYao=w1428-h1903-no'
+    face_ids = face_reg.detect_faces(img_url)
+    for face_id in face_ids:
+        name = sql.get_name_from_fid(face_id)
+        if not (name == 'Cora'):
+            return HttpResponse({
+                'name' : name
+            })
 
 '''
 def get_face_ids_from_db(request):
